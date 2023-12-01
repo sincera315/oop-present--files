fe0e9cabd34dfde84a3c5da86451570b6f7e4019
sincera315 <142091493+sincera315@users.noreply.github.com>
2023-12-01 15:14:36 +0500

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -1,19 +1,21 @@
 #pragma once
 #include "User.h"
 #include "Menu.h"
+#include<string>
 using namespace std;
 
 class Customer : protected User
 {
-
+protected:
+    string type;
 public:
-    
+
     Customer();
     ~Customer();
     void ViewMenu() const;
     void PlaceOrder() const;
     void view_order_history() const;
     void log_out() const;
-    void rate() const;
+    //void rate() const;
 
 };
