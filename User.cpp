04dc33e992437a1a3897b0ab5f0f7396285b1943
sincera315 <142091493+sincera315@users.noreply.github.com>
2023-12-09 21:51:20 +0500

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,43 +1,25 @@
 #include "User.h"
+#include<iostream>
+#include <fstream>
+using namespace std;
+
 
 int User::userCount = 1;
-
-User::User() 
+User::User() : menu(nullptr), userID(++userCount), userName(""), password(""), userType("")
 {
-    cout << "Enter User Name: ";
-    cin >> userName;
-
-    cout << "Enter User Password: ";
-    cin >> password;
-
-    cout << "Enter User Type (student, faculty, staff, admin): ";
-    cin >> userType;
-
-    userCount++;
-    userID = userCount;
 }
 
-User::~User() {
-    // Destructor implementation
-}
-User::User(const string& userName, const string& password, const string& userType) : userName(userName), password(password), userType(userType) {}
-
-void User::displayInfo() const
+User::~User() 
 {
-    cout << "User ID: " << userID << ", User Name: " << userName << ", User Type: " << userType << endl;
-}
-
-void User::showMenu() const
-{
-    cout << "Default Menu: No specific options for this user type." << endl;
-}
-
-const string& User::getUserType() const
-{
-    return userType;
-}
-
-const int User::getuserid() const {
-    return userID;
+    delete menu;
 }
 //file handling login, register
+void User::login(const std::string& enteredUserName, const std::string& enteredPassword)
+{
+
+}
+
+void User::registerUser(const std::string& newUserName, const std::string& newPassword, const std::string& newUserType)
+{
+   
+}
